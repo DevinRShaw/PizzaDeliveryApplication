4cfc530a5cbd662818c4c786ae35180902d79a28
Devin Shaw <dshaw013@xe-10.cs.ucr.edu>
2024-11-12 14:34:41 -0800

    added item and updated primary key for user

--- scripts/entityCreation.sql
+++ scripts/entityCreation.sql
@@ -1,5 +1,5 @@
 CREATE TABLE User (
-    login CHAR(15) NOT NULL UNIQUE,
+    login CHAR(15) PRIMARY KEY,
     password CHAR(50) NOT NULL, 
     phoneNumber CHAR(60) NOT NULL,
     role CHAR(10) NOT NULL,
@@ -7,8 +7,12 @@ CREATE TABLE User (
 );
 
 CREATE TABLE Item (
-
-
+    itemName CHAR(50) PRIMARY KEY,
+    type CHAR(40) NOT NULL,
+    price float NOT NULL, 
+    ingredients CHAR(500) NOT NULL,
+    description CHAR(600),
+    imageURL CHAR(256)
 );
 
 CREATE TABLE Store (
