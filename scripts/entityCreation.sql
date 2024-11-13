df86e37a7127d0d65248f5c4886b5f430220c883
Mailyn Nguyen <mnguy602@xe-10.cs.ucr.edu>
2024-11-12 20:37:26 -0800

    finsihed entities

--- scripts/entityCreation.sql
+++ scripts/entityCreation.sql
@@ -1,4 +1,13 @@
-CREATE TABLE User (
+DROP TABLE IF EXISTS P_User CASCADE;
+DROP TABLE IF EXISTS Item CASCADE;
+DROP TABLE IF EXISTS Store CASCADE;
+DROP TABLE IF EXISTS P_Order CASCADE;
+DROP TABLE IF EXISTS has;
+DROP TABLE IF EXISTS views;
+DROP TABLE IF EXISTS availableAt;
+
+
+CREATE TABLE P_User (
     login CHAR(15) PRIMARY KEY,
     password CHAR(50) NOT NULL, 
     phoneNumber CHAR(60) NOT NULL,
@@ -16,6 +25,48 @@ CREATE TABLE Item (
 );
 
 CREATE TABLE Store (
+    storeID char(50) PRIMARY KEY,
+    address char(40) NOT NULL,
+    city char(40) NOT NULL,
+    state char(40) NOT NULL,
+    isOpen char(40) NOT NULL,
+    reviewStore float
+);
 
+CREATE TABLE P_Order (
+    orderID char(60) UNIQUE NOT NULL,
+    orderTimestamp timestamp NOT NULL,
+    orderStatus char(50) NOT NULL,
+    totalPrice float NOT NULL,
+    userLogin CHAR(15) NOT NULL, -- User mandatory
+    storeID CHAR(50) NOT NULL, -- Store mandatory
+    PRIMARY KEY(orderID),
+    FOREIGN KEY (userLogin) REFERENCES P_User(login),
+    FOREIGN KEY (storeID) REFERENCES Store(storeID)
+);
 
+-- how to do the total participation 
+-- constraint on Order?
+CREATE TABLE has (
+    orderID CHAR(60) UNIQUE NOT NULL,
+    storeID CHAR(50) UNIQUE NOT NULL,
+    PRIMARY KEY(orderID, storeID),
+    FOREIGN KEY (orderID) REFERENCES P_Order(orderID),
+    FOREIGN KEY (storeID) REFERENCES Store(storeID)
+);
+
+CREATE TABLE views (
+    userLogin CHAR(15) UNIQUE NOT NULL,
+    itemName CHAR(50) UNIQUE NOT NULL,
+    PRIMARY KEY(userLogin, itemName),
+    FOREIGN KEY (userLogin) REFERENCES P_User(login),
+    FOREIGN KEY (itemName) REFERENCES Item(itemName)
+);
+
+CREATE TABLE availableAt (
+    itemName CHAR(50) UNIQUE NOT NULL,
+    storeID CHAR(50) UNIQUE NOT NULL,
+    PRIMARY KEY(itemName, storeID),
+    FOREIGN KEY (storeID) REFERENCES Store(storeID),
+    FOREIGN KEY (itemName) REFERENCES Item(itemName)
 );
