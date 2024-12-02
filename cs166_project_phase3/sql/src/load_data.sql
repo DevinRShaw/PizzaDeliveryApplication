8470b531032c24b95ac9b5ab606a04309ca5a701
Mailyn Nguyen <mnguy602@xe-10.cs.ucr.edu>
2024-12-02 11:06:37 -0800

    merge

--- cs166_project_phase3/sql/src/load_data.sql
+++ cs166_project_phase3/sql/src/load_data.sql
@@ -1,20 +1,20 @@
 /* Replace the location to where you saved the data files*/
 COPY Users
-FROM 'REPLACE PATH HERE/users.csv'
+FROM '/home/csmajs/mnguy602/PizzaDeliveryApplication/cs166_project_phase3/data/users.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Items
-FROM 'REPLACE PATH HERE/items.csv'
+FROM '/home/csmajs/mnguy602/PizzaDeliveryApplication/cs166_project_phase3/data/items.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Store
-FROM 'REPLACE PATH HERE/store.csv'
+FROM '/home/csmajs/mnguy602/PizzaDeliveryApplication/cs166_project_phase3/data/store.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY FoodOrder
-FROM 'REPLACE PATH HERE/foodorder.csv'
+FROM '/home/csmajs/mnguy602/PizzaDeliveryApplication/cs166_project_phase3/data/foodorder.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY ItemsInOrder
-FROM 'REPLACE PATH HERE/itemsinorder.csv'
+FROM '/home/csmajs/mnguy602/PizzaDeliveryApplication/cs166_project_phase3/data/itemsinorder.csv'
 WITH DELIMITER ',' CSV HEADER;
